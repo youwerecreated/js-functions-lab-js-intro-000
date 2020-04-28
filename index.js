14a3bfe2fd3fe34e2232fa4260fc9d9169384ed7
Tina Murphy <tina.kep@me.com>
2020-04-28 19:14:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays ()
+  console.log {"Happy" + "holidays!"};
+
+function happyHolidaysTo {
+  console.log (`Happy holidays, ${name}!`)
+}
+
+function happyCustomHoldidayTo
+console.log(`Happy ${holiday}, ${name}!`);
